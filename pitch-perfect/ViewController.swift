ffcd2987d2a4ff9712e550580a2334d3d4167069
Davit Mirzoyan <davit.mirzoyan@yahoo.com>
2018-12-27 08:02:49 +0400

    Initial Commit

---
+++ pitch-perfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pitch-perfect
+//
+//  Created by Davit Mirzoyan on 12/27/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
