5343c38f08246ba8ea290e3ba05a863dd8399155
Davit Mirzoyan <davit.mirzoyan@yahoo.com>
2019-01-06 01:58:23 +0400

    Added sound effects on audio

--- pitch-perfect/SoundPlayerViewController.swift
+++ pitch-perfect/SoundPlayerViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundPlayerViewController: UIViewController {
     
@@ -19,19 +20,52 @@ class SoundPlayerViewController: UIViewController {
     @IBOutlet weak var stopButton: UIButton!
     
     var recordedAudioUrl: URL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
     
     @IBAction func playSoundForButton(_ sender: UIButton) {
-        print("Play sound pressed")
+        
+        guard let buttonTag = ButtonType(rawValue: sender.tag)
+        else { return }
+        
+        switch buttonTag {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(.playing)
     }
     
     @IBAction func stopButtonPressed(_ sender: UIButton) {
-        print("Stop button pressed")
+        stopAudio()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         addButtonConstraints()
+        setupAudio()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
     }
     
     private func addButtonConstraints() {
